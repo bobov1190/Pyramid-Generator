6d236e861f03636a23b10424728823e2695df007
Bryan <109802068+bobov1190@users.noreply.github.com>
2024-08-07 11:03:35 +0500

    script.js

--- script.js
+++ script.js
@@ -1,48 +1,37 @@
-/* Step 1
-In this project, you will learn the basics of Javascript and apply those concepts to building a pyramid generator.
+const character = '#'
+const count = 8
+const rows = []
+let inverted = true
 
-A pyramid generator is a program where you can set the type of character, the count for the pyramid, and the direction of the pyramid. The program will then generate a pyramid based on those inputs.
+function padRow(rowNumber, rowCount) {
+	return (
+		' '.repeat(rowCount - rowNumber) +
+		character.repeat(2 * rowNumber - 1) +
+		' '.repeat(rowCount - rowNumber)
+	)
+}
 
-console.log("Let's build a pyramid using JavaScript basics:")
-console.log('    #    ')
-console.log('   ###   ')
-console.log('  #####  ')
-console.log(' ####### ')
-console.log('#########')
-*/
+// TODO: use a different type of loop
+for (let i = 1; i <= count; i++) {
+	if (inverted) {
+		rows.unshift(padRow(i, count))
+	} else {
+		rows.push(padRow(i, count))
+	}
+}
 
-/* Step 2
-Use the let keyword to declare a variable called character.
+/*while (rows.length < count) {
+  rows.push(padRow(rows.length + 1, count));
+}*/
 
-Note: It is common practice to end statements in JavaScript with a semicolon. ;
+/*for (let i = count; i > 0; i--) {
+  rows.push(padRow(i, count));
+}*/
 
-let character
-*/
+let result = ''
 
-/* Step 3
-Assigning a value to a variable at the moment of its declaration is known as initialization.
+for (const row of rows) {
+	result = result + '\n' + row
+}
 
-Initialize your character variable by assigning it the value "Hello" during its declaration.
-*/
-
-/* Step 4
-JavaScript has seven primitive data types, with String being one of them. In JavaScript, a string represents a sequence of characters and can be enclosed in either single (') or double (") quotes.*Note that strings are immutable, which means once they are created, they cannot be changed. The variable can still be reassigned another value.
-*/
-
-/* Step 5
-The console allows you to print and view JavaScript output. You can send information to the console using console.log().
-*/
-
-/* Step 6
-When a variable is declared with the let keyword, you can reassign (or change the value of) that variable later on.
-Note that when reassigning a variable, you do not use the let keyword again.
-*/
-
-/* Step 7
-Just a console.log()
-*/
-
-/* Step 8
-When variable names are more than one word, there are specific naming conventions for how you capitalize the words. In JavaScript, the convention to use is camel case.
-Camel case means that the first word in the name is entirely lowercase, but the following words are all title-cased. Here are some examples of camel case
-*/
+console.log(result)
